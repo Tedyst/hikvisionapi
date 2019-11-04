c4fb4c83b3d90b12f9cba9e712f9f40124fc3dab
Stoica <stoicatedy@gmail.com>
2019-11-04 12:44:15 +0200

    added requirements.txt

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-import hikvisionapi.hikvisionapi.utils
+import hikvisionapi.utils
 from collections import OrderedDict
 
 
@@ -19,7 +19,7 @@ def test_xml2dict():
     <telecontrolID>1</telecontrolID>
 </DeviceInfo>
 """
-    dicti = hikvisionapi.hikvisionapi.utils.xml2dict(xmlorig)
+    dicti = hikvisionapi.utils.xml2dict(xmlorig)
     dictiorig = {'DeviceInfo': OrderedDict([('deviceName', 'DVR'), ('deviceID', 'someID'), ('model', 'DS-7208HUHI-F2/N/A'), ('serialNumber', 'someID'), ('macAddress', 'aa:bb:cc:dd:ee:ff'),
                                             ('firmwareVersion', 'V3.4.88'), ('firmwareReleasedDate', 'build180228'), ('encoderVersion', 'V5.0'), ('encoderReleasedDate', 'build180126'), ('deviceType', 'IPC'), ('telecontrolID', '1'), ('@attrs', {'version': '1.0', 'asd': '2.0'})])}
     assert dicti == dictiorig
@@ -29,6 +29,6 @@ def test_dict2xml():
     dicti = {'DeviceInfo': OrderedDict([('deviceName', 'DVR'), ('deviceID', 'someID'), ('model', 'DS-7208HUHI-F2/N/A'), ('serialNumber', 'someID'), ('macAddress', 'aa:bb:cc:dd:ee:ff'),
                                         ('firmwareVersion', 'V3.4.88'), ('firmwareReleasedDate', 'build180228'), ('encoderVersion', 'V5.0'), ('encoderReleasedDate', 'build180126'), ('deviceType', 'IPC'), ('telecontrolID', '1'), ('@attrs', {'version': '1.0', 'asd': '2.0'})])}
     xmlorig = """<?xml version = "1.0" encoding = "UTF-8" ?><DeviceInfo asd="2.0" version="1.0" xmlns="http://www.hikvision.com/ver20/XMLSchema"><deviceName>DVR</deviceName><deviceID>someID</deviceID><model>DS-7208HUHI-F2/N/A</model><serialNumber>someID</serialNumber><macAddress>aa:bb:cc:dd:ee:ff</macAddress><firmwareVersion>V3.4.88</firmwareVersion><firmwareReleasedDate>build180228</firmwareReleasedDate><encoderVersion>V5.0</encoderVersion><encoderReleasedDate>build180126</encoderReleasedDate><deviceType>IPC</deviceType><telecontrolID>1</telecontrolID></DeviceInfo>"""
-    newxml = hikvisionapi.hikvisionapi.utils.dict2xml(dicti)
+    newxml = hikvisionapi.utils.dict2xml(dicti)
     print(newxml)
     assert str(xmlorig) == str(newxml)
